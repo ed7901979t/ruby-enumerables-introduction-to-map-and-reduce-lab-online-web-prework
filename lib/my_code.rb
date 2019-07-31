0372e32999f55df8b6386c1689e5043f8b73157b
clean-command-1851 <ed7901979t@gmail.com>
2019-07-31 23:06:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array[]
+  source_array.map { |n| n*(-1)     }
+end
+ 
+map_to_negativize(numbers)
+
+def map_to_no_change(source_array)
+  source_array[] 
+  source_array.map {|n| }
+end
+
+def map_to_double(source_array)
+  source_array[]
+  source_array.map { |n| n*2}
+end
+
+def map_to_square(source_array)
+  source_array[]
+  source_array.map { |n| n.Math.sqrt    }
+end
+
